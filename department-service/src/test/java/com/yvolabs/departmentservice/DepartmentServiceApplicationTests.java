0cd3c1bc98de1c58406f97a20fbf51c2e4a94ec5
yvonnkr <yvonnkr86@gmail.com>
2024-03-20 15:22:13 +0000

    add department-service module

---
+++ department-service/src/test/java/com/yvolabs/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
